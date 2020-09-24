870fe08b28d89b511cd8fe5d51f7408fc3523919
ohsyln <sylnnnn@gmail.com>
2020-09-25 00:51:54 +0800

    support absolute/relative href paths in <a> tags

--- views.py
+++ views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-    
 from django.http import HttpResponse
 
-from .models import Greeting
-
 import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 def index(request):
   # Get URL of page to visit
@@ -14,6 +12,28 @@ def index(request):
   r = requests.get(url)
   html = r.content
 
+  # Parse html using BeautifulSoup
+  soup = BeautifulSoup(html, 'html.parser')
+
+  # Modify href relative links to absolute links
+  parsed_uri = urlparse(url)
+  result = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
+  PREPEND = 'https://access-blocked-sites.herokuapp.com?url={}'.format(result)
+  PREPEND_REL = 'https://access-blocked-sites.herokuapp.com?url={}'.format(url)
+  links = soup.find_all('a')
+  for link in links:
+    try:
+      href = link.get('href')
+      # href = "/a/b/c" , i.e. absolute path
+      if 'http' not in href and href.startswith('/'):
+        link['href'] = PREPEND + href
+      # href = "./a/b/c" OR "a/b/c" , i.e. relative path
+      else:
+        link['href'] = PREPEND_REL + href
+    except:
+      # if any <a> tag fails to parse, ignore that tag
+      continue
+
   # Return page
-  return HttpResponse(html)
+  return HttpResponse(str(soup))
 
